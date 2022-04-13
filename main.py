94f70f98fb005a9a1c50261123edb67d86e90822
Deivid <deividmg02@gmail.com>
2022-04-12 20:59:48 -0600

    Lista de administradores y diccionario de carreras

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print("Hola")
+administradores = [
+    {
+    'nombre_completo':'Gabriel Alfonso Blanco Ruiz',
+    'telefono': '87657890',
+    'usuario': 'gblanco',
+    'contrasena': '192Gabriel_168'
+    },
+    {
+    'nombre_completo':'Jorge José Hernandez Pérez',
+    'telefono': '87604152',
+    'usuario': 'jquiros',
+    'contrasena': 'jorginho0777'
+    },
+    {
+    'nombre_completo':'Daniel Josué Quirós Ampieé',
+    'telefono': '88024367',
+    'usuario': 'dquiros',
+    'contrasena': 'daniTurtle00'
+    },
+    {
+    'nombre_completo':'Sofía Jimena Alfaro Jiménez',
+    'telefono': '89076444',
+    'usuario': 'salfaro',
+    'contrasena': 'minny02x02'
+    }
+]
+
+carreras = {
+    'Administración de Empresas':['Computación Para Administración','Contabilidad I',
+    'Metodología De La Investigación','Derecho Comercial','Cálculo Para Administración'],
+
+    'Gestión del Turismo Rural Sostenible':['Desarrollo De Emprendedores','Fundamentos De Ecología','Legislación Turística De Costa Rica','Servicios Turísticos Reales','Gestión Ambiental Y Turismo'],
+
+    'Ingeniería En Computación':['Introducción A La Programación','Taller De Programación','Estructura De Datos','Programación Orientada A Objetos','Arquitectura De Computadores'],
+    
+    'Ingeniería Electrónica':['Elementos De Computación','Introducción A La Ingeniería','Dibujo Técnico','Seguridad Y Salud Ocupacional','Analisis Y Diseño De Algoritmos'],
+    
+    'Ingeniería En Agronomía':['Introducción A La Agronomía','Agromática I','Botánica General','Introducción A Plagas Agropecuarias','Fisiología Vegetal']
+} #Faltaría agregar más si el profe pide más
